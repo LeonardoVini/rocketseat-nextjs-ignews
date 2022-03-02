949f56dcc38b3ad42632ffd7fe99a91db4613237
leooliveira <leooliveira@equinix.com>
2022-03-02 17:33:05 -0300

    Initial Commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,43 @@
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data, status } = useSession()
+  const router = useRouter()
+
+  async function handleSubscriber() {
+    if (status === 'unauthenticated') {
+      signIn('github')
+      return;
+    }
+
+    if (data.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  return (
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscriber}>
+      Subscribe now
+    </button>
+  );
+}
